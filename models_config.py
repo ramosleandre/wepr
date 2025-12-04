b3761b2b368632355bc40fbc7f552fcc4de01e88
linguini <leandre.ramos@epitech.eu>
2025-12-04 23:19:00 +0100

    update model list

--- models_config.py
+++ models_config.py
@@ -4,17 +4,25 @@
 AVAILABLE_MODELS = [
     {
         'id': 'cas/ministral-8b-instruct-2410_q4km',
-        'name': 'Ministral 8B (Default)'
+        'name': 'Ministral 8B'
     },
     {
         'id': 'gemma3:4b',
-        'name': 'Gemma 4B'
+        'name': 'Hand•e : Gemma 4B (Default)'
     },
     {
         'id': 'llama3.2:1b',
         'name': 'Llama 3.2 1B'
     },
+    {
+        'id': 'gpt-oss:20b',
+        'name': ' Hand•e: gpt-oss 20B'
+    },
+    {
+        'id': 'qwen3:latest',
+        'name': 'Hand•e: qwen3 latest'
+    },
     # Add more models here as needed
 ]
 
-DEFAULT_MODEL = 'cas/ministral-8b-instruct-2410_q4km'
+DEFAULT_MODEL = 'gemma3:4b'
